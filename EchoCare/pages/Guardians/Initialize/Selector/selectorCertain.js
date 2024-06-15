bca7e7e00f20e0483865cdd412daaaf0a65e9088
bunnyoii <Liushuyi1127@outlook.com>
2024-06-15 22:00:57 +0800

    1

--- EchoCare/pages/Guardians/Initialize/Selector/selectorCertain.js
+++ EchoCare/pages/Guardians/Initialize/Selector/selectorCertain.js
@@ -20,62 +20,14 @@ Page({
   },
 
   onConfirm: function() {
-    console.log(options); // 打印查看传递过来的所有参数
+    console.log("用户名: " + this.data.userName); // 正确使用在data中存储的用户名
+    console.log("用户头像URL: " + this.data.userImage); // 正确使用在data中存储的用户头像URL
     // 确定按钮的逻辑处理
     wx.navigateTo({
-      url: '/pages/home/home', // 假设点击确定后返回首页
+      url: '/pages/Guardians/Initialize/Choose/chooseFunctions', // 假设点击确定后导航到新页面
       fail: function(error) {
         console.log("跳转失败:", error);  // 查看跳转失败的错误信息
       }
     });
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide() {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload() {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh() {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom() {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage() {
-
-  }
+  }  
 })
